1108f4c137740029266878dda09e83db8b96b5dc
zjianhui08 <z_jianhui@163.com>
2019-12-09 17:02:29 +0800

    find

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import React from 'react'
 import {render} from 'react-dom'
+import {BrowserRouter as Router,Route,Switch} from "react-router-dom"
 import App from './App'
-render(<App />,document.getElementById('root'))
+import {mainRoute} from './routes'
+
+render(
+  console.log(mainRoute)
+(<Router>
+  <Switch>
+    <App></App>
+  {
+    mainRoute.map(router => {
+      return <Route exact key={router.pathname} path={router.pathname} component={router.component}/>
+    })
+  }
+  </Switch>
+</Router>)
+
+,document.getElementById('root'))
+
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 // 该文件是路由表的文件
 // 将pages的index.js中的暴露的组件引入
 
-// import {Home , Classify , Find , Shopcar ,  My , NotFound} from '../pages'
+import {Find} from '../pages'
 // // 将主路由暴露出去
-// export const mainRoute = [{
+export const mainRoute = [
+// {
 //   pathname: '/home',
 //   component : Home
 // },
@@ -11,10 +12,10 @@
 //   pathname: '/classify',
 //   component : Classify
 // },
-// {
-//   pathname: '/find',
-//   component : Find
-// },
+{
+  pathname: '/find',
+  component : Find
+},
 // {
 //   pathname: '/shopcar',
 //   component : Shopcar
@@ -27,4 +28,4 @@
 //   pathname: '/notFound',
 //   component : NotFound
 // }
-// ]
+]
