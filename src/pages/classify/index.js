179263290d33120bd92704aba52df20c7134def3
SWfromShandong <670562245@qq.com>
2019-12-09 16:23:52 +0800

    add classify 0.0

---
+++ src/pages/classify/index.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react'
+
+class Classify extends Component{
+    render(h) {
+        return(
+            <div>傻嘚</div>
+        )
+    }
+}
+
+export default Classify
