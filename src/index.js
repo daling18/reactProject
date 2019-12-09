b5b6cd6be2a0ec81bd9dc304ac2b6a1a7a8c192e
daling18 <54314033+daling18@users.noreply.github.com>
2019-12-09 22:34:17 +0800

    two

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
 import {render} from 'react-dom'
 import App from './App'
+import '@/assets/css/App.css'
+import './assets/setRem'
 render(<App />,document.getElementById('root'))
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,33 @@ import Loabable from 'react-loadable'
 // 引入loading的组件
 import Loading from "../component/Loading"
 
-const NotFound = Loabable ({
-  loader : () => import('需要加效果的组件'),
+const Home = Loabable ({
+  loader : () => import('./home'),
+  loading:Loading
+})
+const Classify = Loabable ({
+  loader : () => import('./classify'),
+  loading:Loading
+})
+const Find = Loabable ({
+  loader : () => import('./find'),
+  loading:Loading
+})
+const My = Loabable ({
+  loader : () => import('./my'),
+  loading:Loading
+})
+const Shopcar = Loabable ({
+  loader : () => import('./shopcar'),
   loading:Loading
 })
 
+
 // 将组件暴露
 export {
-  NotFound 
+  Home,
+  Classify,
+  Find,
+  Shopcar,
+  My
 }
