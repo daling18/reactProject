78c27b520b768fe2de403f4bc43c0de82d808561
y-1948 <54272799+y-1948@users.noreply.github.com>
2019-12-13 11:31:39 +0800

    Revert "one"
    This reverts commit bc7f1806fd3ceffc5fbb0e0e9853acf6af12c751.

--- src/pages/home/reducer/index.js
+++
@@ -1,6 +0,0 @@
-const deState={
-    a:1
-}
-export const text=(state=deState,action)=>{
-    return deState
-}
--- src/store/reducer/index.js
+++
@@ -1,7 +0,0 @@
-import {combineReducers} from 'redux'
-// 引入各个组件的store文件
-import reducerFormHome from '../../pages/home/reducer'
-const reducer = combineReducers ({
-  reducerFormHome,
-})
-export default reducer
